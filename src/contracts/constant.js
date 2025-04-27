6d4595e8d1b5f67070e13d207b1e1e040b89e672
0xClint <omkarbdarde@gmail.com>
2025-04-27 15:57:58 +0530

    price feed added

--- src/contracts/constant.js
+++ src/contracts/constant.js
@@ -1,6 +1,9 @@
 export const SRN_CONTRACT_ADDRESS =
   "0x5f3630e2DCAAF215d9b720886F9D652EBC3fB957";
 
+export const PRICE_FEED_CONTRACT_ADDRESS =
+  "0xc4842D8E5f639B1AA9c4070cFe9aE0FF24d80ae4";
+
 export const SRN_CONTRACT_ABI = [
   {
     inputs: [],
@@ -25,6 +28,105 @@ export const SRN_CONTRACT_ABI = [
         internalType: "uint256",
         name: "timestamp",
         type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
+
+export const PRICE_FEED_CONTRACT_ABI = [
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "feedIds",
+    outputs: [
+      {
+        internalType: "bytes21",
+        name: "",
+        type: "bytes21",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "flrUsdId",
+    outputs: [
+      {
+        internalType: "bytes21",
+        name: "",
+        type: "bytes21",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getFlrUsdPrice",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+      {
+        internalType: "int8",
+        name: "",
+        type: "int8",
+      },
+      {
+        internalType: "uint64",
+        name: "",
+        type: "uint64",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getFlrUsdPriceWei",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+      {
+        internalType: "uint64",
+        name: "",
+        type: "uint64",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getFtsoV2CurrentFeedValues",
+    outputs: [
+      {
+        internalType: "uint256[]",
+        name: "_feedValues",
+        type: "uint256[]",
+      },
+      {
+        internalType: "int8[]",
+        name: "_decimals",
+        type: "int8[]",
+      },
+      {
+        internalType: "uint64",
+        name: "_timestamp",
+        type: "uint64",
       },
     ],
     stateMutability: "view",
--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -3,6 +3,8 @@ import {
   initWalletClient,
 } from "../../components/walletConnect";
 import {
+  PRICE_FEED_CONTRACT_ABI,
+  PRICE_FEED_CONTRACT_ADDRESS,
   SRN_CONTRACT_ABI,
   SRN_CONTRACT_ADDRESS,
 } from "../../contracts/constant";
@@ -49,6 +51,50 @@ document.getElementById("slide-wrapper").addEventListener("click", (e) => {
   }
 });
 
+const priceFeedArr = [
+  { id: 1, title: "FLR/USD" },
+  { id: 2, title: "BTC/USD" },
+  { id: 3, title: "ETH/USD" },
+];
+
+const getPriceFeed = async () => {
+  let data = await publicClient.readContract({
+    address: PRICE_FEED_CONTRACT_ADDRESS,
+    abi: PRICE_FEED_CONTRACT_ABI,
+    functionName: "getFtsoV2CurrentFeedValues",
+  });
+
+  let priceFeedData = priceFeedArr;
+  for (let i = 0; i < data[0].length; i++) {
+    priceFeedData[i].price = Number(data[0][i]) / Math.pow(10, data[1][i]);
+  }
+
+  const priceFLR = document.getElementById("price-feed-flr");
+  priceFLR.innerHTML = `<b>${priceFeedData[0].price} ${priceFeedData[0].title}</b>`;
+  const priceBTC = document.getElementById("price-feed-btc");
+  priceBTC.innerHTML = `<b>${priceFeedData[1].price} ${priceFeedData[1].title}</b>`;
+  const priceETH = document.getElementById("price-feed-eth");
+  priceETH.innerHTML = `<b>${priceFeedData[2].price} ${priceFeedData[2].title}</b>`;
+
+  console.log(priceFeedData);
+};
+getPriceFeed();
+
+let priceflag = false;
+document.getElementById("price-feed-btn").addEventListener("click", () => {
+  if (!priceflag) {
+    document.getElementById("price-feed-btn").style.transform =
+      "translateX(210px) rotate(180deg)";
+    document.getElementById("price-feed-eth").style.display = "block";
+    document.getElementById("price-feed-btc").style.display = "block";
+  } else {
+    document.getElementById("price-feed-btn").style.transform =
+      "translateX(210px) rotate(0deg)";
+    document.getElementById("price-feed-btc").style.display = "none";
+    document.getElementById("price-feed-eth").style.display = "none";
+  }
+  priceflag = !priceflag;
+});
 
 document.getElementById("setting-btn").addEventListener("click", () => {
   document.querySelector(".modal-container").style.display = "flex";
